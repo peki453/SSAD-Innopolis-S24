3a35110a868c36f7050c2895c16003e64b1a04d2
Marko Pezer <markopezer@gmail.com>
2024-01-27 23:39:43 +0300

    Create Task2b.cpp

---
+++ Week01/Task2b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int& a, int& b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int x = 3, y = 5;
+
+    cout << "Before swapping: " << endl;
+    cout << "a = " << x << "; b = " << y << endl;
+
+    swap(x, y);
+
+    cout << "After swapping: " << endl;
+    cout << "a = " << x << "; b = " << y << "\n";
+
+    return 0;
+}
