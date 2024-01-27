befca56fc0ef6039059b9c0487a9c4ff4968302e
Marko Pezer <markopezer@gmail.com>
2024-01-27 23:49:01 +0300

    Create Task3a.cpp

---
+++ Week01/Task3a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int A[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int flag = 0;
+        for (int j = i - 1; j >= 0; j--) {
+            if (A[i] == A[j]) flag = 1;
+        }
+        if (!flag) cout << A[i] << " ";
+    }
+    cout << endl;
+}
